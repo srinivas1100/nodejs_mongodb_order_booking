df6d3848ea5253940970179b4c241adad008a833
srinu <srinu11112000@gmail.com>
2022-06-13 13:31:27 +0530

    create user, get all users

--- server.js
+++ server.js
@@ -1,20 +1,22 @@
 const express = require("express");
-const {DataBase, Use} = require("./src/database/database");
-const user = require('./src/routes/user_routes');
+const { DataBase, Use } = require("./src/database/database");
+const user = require("./src/routes/user_routes");
 
 const app = express();
 
-//app.use(express.json());
+app.use(express.json());
 
-app.use('/v1', user);
+app.get("/user", (req, res) => {
+  Use.find({})
+    .then((a) => {
+      res.send(a);
+    })
+    .catch((err) => {
+      res.send(err);
+    });
+});
 
-app.get('/user', (req, res) => {
-   Use.find({}).then((a) =>{
-    res.send(a);
-   }).catch((err) =>{
-    res.send(err)
-   });
-})
+app.use("/v1", user);
 
 app.listen(4444, () => {
   console.log("server run in port 4444");
--- src/routes/user_routes.js
+++ src/routes/user_routes.js
@@ -3,13 +3,49 @@ const userModel = require("../models/user/user_model");
 
 const router = express.Router();
 
-router.post("/user", (req, res) => {
-  const user = new userModel.create({
-    //   userId: "200",
-    name: "srinu",
+// create a new user
+router.post("/user", async (req, res) => {
+  const user = await userModel({
+    userName: req.body.userName,
+    password: req.body.password,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    telePhone: req.body.telePhone,
   });
-  user.save();
-  res.status(200).send(user);
+  user
+    .save()
+    .then((a) => {
+      console.log(a);
+      res.status(200).send(a);
+    })
+    .catch(() => {
+      console.log(err);
+      res.status(404).send(err);
+    });
 });
+
+//get all users in database
+router.get("/getAllUsers", async (req, res) => {
+  const allUsers = await userModel.find({});
+  try {
+    res.status(200).send(allUsers);
+  } catch (error) {
+    console.log(error);
+    res.status(400).send(error);
+  }
+});
+
+// //get user find by id
+// router.get('/user/:id',async (req, res) => {
+//   const id = 
+ 
+//   try {
+//     const user = await userModel.findById({});
+//     res.status(200).send(user);
+//   } catch (error) {
+//     console.log(error);
+//     res.status(400).send(error);
+//   }
+// })
 
 module.exports = router;
